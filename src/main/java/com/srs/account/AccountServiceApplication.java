2332dbcc684b6124e667c65f21ec6b099aa08702
BANVIENCORP\hoang.ngo <hoang.ngo@banvien.com.vn>
2022-10-08 11:25:20 +0700

    Config docker compose for eureka server

--- src/main/java/com/srs/account/AccountServiceApplication.java
+++ src/main/java/com/srs/account/AccountServiceApplication.java
@@ -4,13 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class AccountServiceApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(AccountServiceApplication.class);
